a7d529aa5b2560e6692adc4c0786f4073b945dd4
SuperAllan <66976270+super-allan@users.noreply.github.com>
2021-10-15 03:01:43 -0300

    orm node

--- src/domain/rules/User.ts
+++ src/domain/rules/User.ts
@@ -1,3 +1,45 @@
-interface UserAttributes { 
-    
+import {
+  Sequelize,
+  Model,
+  Optional,
+  DataTypes,
+} from "sequelize";
+
+interface UserAttributes {
+  id: number;
+  name: string;
+  preferredName: string | null;
 }
+
+const sequelize = 
+
+interface UserCreationAttributes extends Optional<UserAttributes, "id"> { }
+
+class SUser extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes {
+  public id!: number; // Note that the `null assertion` `!` is required in strict mode.
+  public name!: string;
+  public preferredName!: string | null; // for nullable fields
+}
+
+SUser.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    preferredName: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "projects",
+  }
+);
